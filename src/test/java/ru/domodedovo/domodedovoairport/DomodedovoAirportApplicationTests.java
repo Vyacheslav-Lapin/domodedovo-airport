3f63820b84cd1522d68b1e3f65762f3ef3a6b804
Vyacheslav Lapin <vyacheslav@y-tree.com>
2020-01-31 14:45:44 +0300

    Test added

--- src/test/java/ru/domodedovo/domodedovoairport/DomodedovoAirportApplicationTests.java
+++ src/test/java/ru/domodedovo/domodedovoairport/DomodedovoAirportApplicationTests.java
@@ -1,13 +1,43 @@
 package ru.domodedovo.domodedovoairport;
 
+import static com.jayway.jsonpath.JsonPath.parse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser(authorities = "ADMIN")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class DomodedovoAirportApplicationTests {
+
+  MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+  @SneakyThrows
+	@Test
+	@DisplayName("Flight works currectly")
+	void flightWorksCuttectlyTest() {
+    mockMvc.perform(get("/flights"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(HAL_JSON_VALUE))
+        .andExpect(mvcResult -> assertEquals(3,
+            parse(mvcResult.getResponse().getContentAsString())
+                .<Integer>read("$.page.totalElements").intValue()));
+	}
 }
