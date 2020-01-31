de6f6c5ad6452e94bfffb8d1ef1589c9c68959a2
Vyacheslav Lapin <vyacheslav@y-tree.com>
2020-01-31 12:24:02 +0300

    Initial commit

---
+++ src/main/java/ru/domodedovo/domodedovoairport/DomodedovoAirportApplication.java
@@ -0,0 +1,15 @@
+package ru.domodedovo.domodedovoairport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomodedovoAirportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomodedovoAirportApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/ru/domodedovo/domodedovoairport/DomodedovoAirportApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.domodedovo.domodedovoairport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomodedovoAirportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
